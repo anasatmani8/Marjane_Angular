20d212aab962c304dba3c6f079bb31145ef88af3
Anas Atmani <anas.atmani@alumni.fh-aachen.de>
2024-02-20 18:23:07 +0100

    admin components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { CustomerComponent } from './pages/admin/customer/customer.component';
+import { CartComponent } from './pages/admin/cart/cart.component';
+import { CategoriesComponent } from './pages/admin/categories/categories.component';
+import { OrderComponent } from './pages/admin/order/order.component';
+import { ProductsComponent } from './pages/admin/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LayoutComponent,
+    CustomerComponent,
+    CartComponent,
+    CategoriesComponent,
+    OrderComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
